7a79bc557e24c512c6dff9eaa16efe295ecd2d7f
lmw <lmwtclmwtc@163.com>
2018-01-10 12:02:17 +0800

    add read json data

--- util/json_util.py
+++ util/json_util.py
@@ -8,3 +8,10 @@ sys.setdefaultencoding('utf-8')
 def write_json_to_file(file_name, data):
     with open(file_name, 'w') as file_object:
         json.dump(data, file_object)
+
+
+def read_json_from_file(file_name):
+    # Reading data back
+    with open(file_name, 'r') as f:
+        data = json.load(f)
+    return data
