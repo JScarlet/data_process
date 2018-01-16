761135e5be3a59049075d8d6cbc78609359ed958
lmw <lmwtclmwtc@163.com>
2018-01-16 23:01:43 +0800

    complete the script for export corpus for api document

--- script/train_fasttext_vector.py
+++ script/train_fasttext_vector.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-import codecs
 import sys
 
 from gensim.models.fasttext import FastText as FT_gensim
@@ -13,9 +12,8 @@ if len(sys.argv) <= 1:
     print "must specific the json file for training fastText word embedding"
     sys.exit(1)
 corpus_file_name = sys.argv[1]
-with codecs.open(corpus_file_name, 'r', 'utf-8') as corpus_file:
-    lee_data = LineSentence(corpus_file)
-    model_gensim = FT_gensim(size=100, min_count=2)
-    model_gensim.build_vocab(lee_data)
-    model_gensim.train(lee_data, total_examples=model_gensim.corpus_count, epochs=model_gensim.iter)
-    model_gensim.save('saved_model_for_jdk_corpus')
+lee_data = LineSentence(corpus_file_name)
+model_gensim = FT_gensim(size=100, min_count=2)
+model_gensim.build_vocab(lee_data)
+model_gensim.train(lee_data, total_examples=model_gensim.corpus_count, epochs=model_gensim.iter)
+model_gensim.save('saved_model_for_jdk_corpus')
